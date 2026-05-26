d1fd9535c63dbdae39bdd8ffceebd23cec472193
DushDev <imthedush@gmail.com>
2026-05-26 21:33:29 +0200

    fix: ultimate CORS bypass for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  // const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: configService.get<string>('FRONTEND_URL') ?? 'http://localhost:5173',
+    origin: true,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
